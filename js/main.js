819b121d40c0939c4afb3ec73dd67cb84b4995a6
willythesillyone <ppin4147@gmail.com>
2025-12-03 22:09:31 +0700

    Update birthday title in main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY 19TH BIRTHDAY LAKSMI').split('')
+    const titles = ('HAPPY DEMISIONER, GISELLE EMANUELLA RUMIMPER').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
