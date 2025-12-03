605d7f7116ac538493fad6f1f73e83d008c4c860
willythesillyone <ppin4147@gmail.com>
2025-12-03 22:22:27 +0700

    Change text displayed in title element

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'HAPPY DEMISIONER, GISELLE EMANUELLA RUMIMPER'.split('')
+const text = 'I Have Something For You, Gis'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
